dbe5097f7600ffd7fc5ccbeb93ec7b0d252b823a
brianjuareza <124235564+brianjuareza@users.noreply.github.com>
2023-02-21 19:37:26 -0600

    Update index.py

--- index.py
+++ index.py
@@ -3,11 +3,11 @@ app = Flask (__name__)
 
 @app.route('/')
 def index_():
- return render_template('index.html')
+ return render_template('login.html')
 
-@app.route('/index.html')
+@app.route('/login.html')
 def index():
- return render_template('index.html')
+ return render_template('login.html')
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=4000)
